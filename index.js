0e4965a70c78710896edc33f51935bb94a02a173
Macluster <deepakjudejohn@gmail.com>
2024-10-10 14:45:09 +0530

    feat:adding portfolio

---
+++ index.js
@@ -0,0 +1,46 @@
+
+function onAboutClicked()
+{
+    var ele=document.getElementsByClassName("hero-section")[0]
+    ele.scrollIntoView({behavior:"smooth"})
+}
+
+function onExpertiesClicked()
+{
+
+    var ele=document.getElementsByClassName("experties-section")[0]
+    ele.scrollIntoView({behavior:"smooth"})
+  
+}
+
+function onProjectsClicked()
+{
+
+    var ele=document.getElementsByClassName("project-section")[0]
+    ele.scrollIntoView({behavior:"smooth"})
+  
+}
+
+function onContactsClicked()
+{
+
+    var ele=document.getElementsByClassName("contacts-section")[0]
+    ele.scrollIntoView({behavior:"smooth"})
+  
+}
+
+function onServicesClicked()
+{
+
+    var ele=document.getElementsByClassName("what-i-do-section")[0]
+    ele.scrollIntoView({behavior:"smooth"})
+  
+}
+
+
+
+
+
+
+
+
